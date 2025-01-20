376460a0fe2f2b96d55edab3d770961844741f76
0xthomas26 <0xthomas26@gmail.com>
2025-01-21 02:40:29 +0900

    [FIX]: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-            </head>
             <body>
                 <Providers>
                     <CssBaseline />
